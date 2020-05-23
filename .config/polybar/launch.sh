018ff8673bcf11363f1a3dddcf27d99fde8c3a76
hebinhao <hebinhao1993@outlook.com>
2020-05-23 21:49:14 +0800

    about polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q polybar
+# killall -q polybar
+# since we do not have killall
+# todo: use built in `command`
+ps -ef |grep polybar |awk '{print $2}'| xargs kill -9
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
