2d2fdba3fbf625b2901c150aa3bed3202e33a7a6
Your Name <you@example.com>
2023-09-09 17:22:51 +0800

    fix typo: yes -> true

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -92,7 +92,7 @@
       # Forbid root login through SSH.
       permitRootLogin = "no";
       # Use keys only. Remove if you want to SSH using password (not recommended)
-      passwordAuthentication = yes;
+      passwordAuthentication = true;
     };
   };
 
