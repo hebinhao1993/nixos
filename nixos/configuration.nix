185a092c4756f9fe1fe8a020dd33f9166fcbd6f3
Your Name <you@example.com>
2023-09-09 17:20:25 +0800

    fix typo

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,7 +88,7 @@
   # Feel free to remove if you don't need it.
   services.openssh = {
     settings = {
-      enable = trues;
+      enable = true;
       # Forbid root login through SSH.
       permitRootLogin = "no";
       # Use keys only. Remove if you want to SSH using password (not recommended)
