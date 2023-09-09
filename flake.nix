7d2cf230a5de2b5c798305a647da13351328609c
Your Name <you@example.com>
2023-09-09 19:50:56 +0800

    home-manager as a NixOS module

--- flake.nix
+++ flake.nix
@@ -25,19 +25,17 @@
       nixvb = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; }; # Pass flake inputs to our config
         # > Our main nixos configuration file <
-        modules = [ ./nixos/configuration.nix ];
-      };
-    };
+        modules = [
+          ./nixos/configuration.nix
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "hbh@nixvb" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-        # > Our main home-manager configuration file <
-        modules = [ ./home-manager/home.nix ];
+          # home-manager as a NixOS module
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.hbh = import ./home-manager/home.nix;
+          }
+        ];
       };
     };
   };
