620f5bbd800c81f0a90e3c3823f353514ff0949e
Your Name <you@example.com>
2023-09-12 00:09:16 +0800

    remove some TODOs

--- hosts/virtualbox/configuration.nix
+++ hosts/virtualbox/configuration.nix
@@ -60,33 +60,23 @@
     };
   };
 
-  # FIXME: Add the rest of your current configuration
-
   time.timeZone = "Asia/Shanghai";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # TODO: Set your hostname
   networking.hostName = "nixvb";
   networking.networkmanager.enable = true;
   networking.firewall.allowedTCPPorts = [ 22 80 443 ];
 
-  # TODO: This is just an example, be sure to use whatever bootloader you prefer
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
-    # FIXME: Replace with your username
     hbh = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
       # initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = [ "wheel" "networkmanager" ];
     };
   };
