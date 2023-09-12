ca0ff2b197ff4518b3ede9b42038c5007feb0f69
Your Name <you@example.com>
2023-09-12 23:42:25 +0800

    modify zsh.nix

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -3,20 +3,17 @@
   home.programs = {
     zsh = {
       enable = true;
-      oh-my-zsh = {
-        enable = true;
-        plugins = [
-          "vi-mode"
-          "jump"
-        ]
-      };
+      dotDir = ;
+      enableAutosuggestions = ;
+      enableCompletion = true;
+      # Extra commands that should be added to .zshenv
       envExtra = ''
 if [ -e ''${HOME}/.nix-profile/etc/profile.d/nix.sh ]; then . ''$HOME/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
       '';
-      #  这部分脚本会在 .zshrc 最前面, 先于 oh-my-zsh 和其他脚本
+      #  这部分脚本会在 .zshrc 最前面
       initExtraFirst = ''
       '';
-      # 这部分脚本优先级低于其他的
+      # 这部分脚本优先级低于其他的，Extra commands that should be added to .zshrc
       initExtra = ''
       '';
     };
