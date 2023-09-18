1b3e0cb9fc4c86cdcd5f9f4a7bee00eb01b5ff5a
Your Name <you@example.com>
2023-09-18 23:51:26 +0800

    ${ has special meaning, should be escaped

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -17,18 +17,18 @@ if [ -e ''${HOME}/.nix-profile/etc/profile.d/nix.sh ]; then . ''$HOME/.nix-profi
 ZIM_HOME=~/.cache/zim
 
 # Download zimfw plugin manager if missing.
-if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+if [[ ! -e ''${ZIM_HOME}/zimfw.zsh ]]; then
   curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
       https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
 fi
 
-# Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
-if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
-  source ${ZIM_HOME}/zimfw.zsh init -q
+# Install missing modules, and update ''${ZIM_HOME}/init.zsh if missing or outdated.
+if [[ ! ''${ZIM_HOME}/init.zsh -nt ''${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+  source ''${ZIM_HOME}/zimfw.zsh init -q
 fi
 
 # Initialize modules.
-source ${ZIM_HOME}/init.zsh
+source ''${ZIM_HOME}/init.zsh
       '';
     };
   };
