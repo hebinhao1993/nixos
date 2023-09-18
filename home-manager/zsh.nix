e16c96bebfb26f1adac1d46c7ec38ffffcdacd0e
Your Name <you@example.com>
2023-09-18 23:30:33 +0800

    add content about zimfw in .zshrc for zsh

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -17,6 +17,21 @@ if [ -e ''${HOME}/.nix-profile/etc/profile.d/nix.sh ]; then . ''$HOME/.nix-profi
       '';
       # 这部分脚本优先级低于其他的，Extra commands that should be added to .zshrc
       initExtra = ''
+ZIM_HOME=~/.cache/zim
+
+# Download zimfw plugin manager if missing.
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+      https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+fi
+
+# Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+  source ${ZIM_HOME}/zimfw.zsh init -q
+fi
+
+# Initialize modules.
+source ${ZIM_HOME}/init.zsh
       '';
     };
   };
