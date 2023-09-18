b53165ba6a35ce8367c13bb23e117f605a3e4066
Your Name <you@example.com>
2023-09-19 00:03:47 +0800

    home.file.<>.source should be path not string

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -35,7 +35,7 @@ source ''${ZIM_HOME}/init.zsh
 
   home.file.".config/zsh/.zimrc" = {
     enable = true;
-    source = "../dotfiles/src/.zimrc";
+    source = ../dotfiles/src/.zimrc;
     target = ".config/zsh/.zimrc";
   };
 }
