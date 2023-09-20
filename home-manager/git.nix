3b049970d5b717e332b991c38ed0c4c0b37a782a
Your Name <you@example.com>
2023-09-20 21:35:09 +0800

    add git aliases

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -4,5 +4,15 @@
     enable = true;
     userName = "hebinhao1993";
     userEmail = "hebinhao1993@outlook.com";
+    aliases = {
+      co = "checkout";
+      ci = "commit";
+      st = "status";
+      br = "branch";
+      p = "pull";
+      f = "fetch";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      last = "log -1 HEAD";
+    };
   };
 }
