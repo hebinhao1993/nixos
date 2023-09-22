0d316b7213e09c562b1fc1dcb32442ab96c3ab13
Your Name <you@example.com>
2023-09-22 23:02:25 +0800

    add config for programs.gnupg

--- hosts/virtualbox/configuration.nix
+++ hosts/virtualbox/configuration.nix
@@ -103,7 +103,14 @@
     wget
     gnupg
     pinentry
+    pinentry-curses
   ];
+
+  programs.gnupg.agent = {
+   enable = true;
+   pinentryFlavor = "curses";
+   enableSSHSupport = true;
+};
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
