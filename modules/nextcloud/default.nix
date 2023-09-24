708cb609cedfa9769ae68fb7704970ff7404ba45
Your Name <you@example.com>
2023-09-24 20:39:29 +0800

    modify module nextcloud

--- modules/nextcloud/default.nix
+++ modules/nextcloud/default.nix
@@ -2,6 +2,18 @@
 {
   services.nginx.virtualHosts."nextcloud.nixvb.com" = {};
 
+  sops.secrets= {
+    "nextcloud/admin_pass" = {
+      sopsFile = ../../secrets/nextcloud/admin.yaml; # bring your own password file
+      owner = config.users.users.nextcloud.name;
+    };
+    "nextcloud/db_pass" = {
+      sopsFile = ../../secrets/nextcloud/db.yaml; # bring your own password file
+      owner = config.users.users.nextcloud.name;
+    };
+  };
+  
+
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud27;
@@ -13,11 +25,11 @@
     config = {
       defaultPhoneRegion = "CN";
       adminuser = "nextcloudamdin"; 
-      adminpassFile = "/etc/nextcloud-admin-password";
+      adminpassFile = config.sops.secrets."nextcloud/admin_pass".path;
       dbtype = "pgsql";
       dbname = "nextcloud";
       dbuser = "nextcloud";
-      dbpassFile = "/etc/nextcloud-db-password";
+      dbpassFile = config.sops.secrets."nextcloud/db_pass".path;
     }; 
   };
 }
