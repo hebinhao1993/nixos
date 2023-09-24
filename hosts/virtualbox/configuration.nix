3aac2f0b8289fd4837a2eb6fadae3cfe880c232f
Your Name <you@example.com>
2023-09-24 19:36:43 +0800

    make gitea configuration as a module

--- hosts/virtualbox/configuration.nix
+++ hosts/virtualbox/configuration.nix
@@ -13,6 +13,7 @@
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
+    ../../modules/gitea
   ];
 
   nixpkgs = {
@@ -95,49 +96,6 @@
       PasswordAuthentication = true;
     };
   };
-
-  sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
-  sops.secrets."postgres/gitea_dbpass" = {
-    sopsFile = ../../secrets/postgres.yaml; # bring your own password file
-    owner = config.users.users.gitea.name;
-  };
-
-  services.gitea = {
-    enable = true; # Enable Gitea
-    appName = "nixvb: Gitea Service"; # Give the site a name
-    database = {
-      type = "postgres"; # Database type
-      passwordFile = config.sops.secrets."postgres/gitea_dbpass".path;
-    };
-    settings.server = {
-      ROOT_URL = "http://gitea.nixvb.com/"; # Root web URL
-      HTTP_PORT = 3001; # Provided unique port
-      DOMAIN = "gitea.nixvb.com"; # Domain name
-    };  
-  };
-
-  services.postgresql = {
-    enable = true;
-    authentication = ''
-      local gitea all ident map=gitea-users
-    '';
-    identMap =                    # Map the gitea user to postgresql
-      ''
-        gitea-users gitea gitea
-      '';
-  };
-
-  services.nginx = {
-    enable = true;
-    recommendedGzipSettings = true;
-    recommendedOptimisation = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-    virtualHosts."gitea.nixvb.com" = {                  # Gitea hostname
-      locations."/".proxyPass = "http://localhost:3001/";   # Proxy Gitea
-    };
-  };
-
 
   environment.systemPackages = with pkgs ; [
     git
