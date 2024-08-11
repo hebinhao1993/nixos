fad154ba68c681b41ab9b5ef35cfdce5a94d021f
Your Name <you@example.com>
2024-08-11 14:39:15 +0800

    nextcloud26 => nextcloud27

--- modules/nextcloud/default.nix
+++ modules/nextcloud/default.nix
@@ -18,7 +18,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud26;
+    package = pkgs.nextcloud27;
     https = false;
     webfinger = true;
     hostName = "nextcloud.nixvb.com";
